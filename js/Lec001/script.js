01e6a23b50994a7e771468c1cca60a98a0f2ff6e
inventerlord <inventerlord@gmail.com>
2021-10-01 16:14:32 +0330

    Js Lec011

---
+++ js/Lec001/script.js
@@ -0,0 +1,40 @@
+// alert("My site says hello word")
+console.log("i am Programmer")
+
+// var
+// let
+// const
+// const inp = document.getElementById('d');
+// console.log(inp.value);
+
+// Data types
+
+// String
+let myname = "Ali";
+console.log(myname +" is "+ typeof(myname));
+// number
+let age = 45.6;
+console.log(age+" is "+typeof(age));
+
+// boolean
+let isActive = true;
+console.log(isActive +" is "+typeof(isActive));
+// object
+let person = {
+    firstname:"Faroogh",
+    secondName:"Ahsna",
+    age:54,
+    greet: function(str = "hello"){
+        console.log(str);
+    }
+}
+console.log(person.firstname);
+console.log(person.greet("Salmaan"));
+// array
+let groc = ["banana","apple",'pencle']
+
+console.log(groc);
+let n = null;
+let data;
+console.log(n);
+console.log(data);
