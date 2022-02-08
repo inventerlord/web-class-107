b21cae4a7711d6c7eb6983908dad08909a270982
inventerlord <inventerlord@gmail.com>
2022-02-08 10:33:08 +0330

    py-lec001

---
+++ python/lec001/lec001.py
@@ -0,0 +1,17 @@
+# datatype
+# 1. string
+# 2. number
+# 3. boolean
+# collection
+# list
+# tuple
+# dict
+
+
+name = "Salmaan"
+age = 25
+isCompleted = True
+frind1 = ["salmaan","ahsan","hamza"] # list
+frind2 = ("salmaan","ahsan","hamza") # tuple
+person = {"name":"salmaan","age":25,"friends":["salmaan","ahsan","hamza"]} #DICT
+
