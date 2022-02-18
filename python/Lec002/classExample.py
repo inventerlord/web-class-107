06b155cdc25d2e99140f23e2ec5db6ddff348174
inventerlord <inventerlord@gmail.com>
2022-02-18 17:31:10 +0330

    p2

---
+++ python/Lec002/classExample.py
@@ -0,0 +1,16 @@
+# import webbrowser
+# webbrowser.open("https://www.google.com")
+import turtle
+
+myt = turtle.Turtle()
+myt.shape("turtle")
+
+
+myt.forward(100)
+myt.left(90)
+myt.forward(100)
+myt.left(90)
+myt.forward(100)
+myt.left(90)
+myt.forward(100)
+input()
