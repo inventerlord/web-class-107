5d4889728b3385cbb5ad41e804fe14ad65c55ca6
inventerlord <inventerlord@gmail.com>
2022-05-15 09:55:36 +0430

    update

--- README.md
+++
@@ -1,9 +0,0 @@
-# web-class-107
- 
-## 1. HTML
-1. Basic Structure
-2. Tags
-
-
-## 2. CSS
-## 3. Javascript
--- django/lec002/blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-@admin.register(models.Post)
-class PostDisplayAdmin(admin.ModelAdmin):
-    list_display = ['title','description']
